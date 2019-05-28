0b57160455a37216a908b616b378b297e6ce0006
Stathis G. Arapostathis <31962431+stathisar@users.noreply.github.com>
2019-05-28 19:16:13 +0300

    Delete 1.data_import_r.R

--- 1.data_import_r.R
+++
@@ -1,7 +0,0 @@
-rm(list = ls())
-#loading kefalonia and lesvos tweets
-
-#Sys.setlocale(category = "LC_ALL", locale = "Greek")
-
-geolocations <- read.csv(file = "/media/stathis/Maxtor/georeference/geolocations.csv")
-kalamos <- read.csv(file = "/media/stathis/Maxtor/georeference/kalamos/predicted.csv")
